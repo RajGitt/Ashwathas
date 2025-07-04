f02d0c53718bf7612b9da6d1ba843285284f8bec
RajGitt <104863610+RajGitt@users.noreply.github.com>
2025-07-04 19:01:33 +0530

    app.py

--- backend/app.py
+++ backend/app.py
@@ -44,6 +44,4 @@ def add_product():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    app.run(host='0.0.0.0', port=10000, debug=True)
